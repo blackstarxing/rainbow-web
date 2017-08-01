e255abeb67bca788e6329b3e47fea15fb7782530
闫佳兴 <yanjx@miqtech.com>
2017-08-01 11:18:41 +0800

    update

--- src/views/recharge.vue
+++ src/views/recharge.vue
@@ -195,16 +195,6 @@
 					});
 	            });
 
-	         	window.onbeforeunload = function() {   
-		            var n = window.event.screenX - window.screenLeft;   
-		            var b = n > document.documentElement.scrollWidth-20;   
-		            if(b && window.event.clientY < 0 || window.event.altKey){   
-		               
-		            }else{
-		                window.location.href = '#/recharge';   
-		            }
-		        }  
-
 	         	// 支付宝支付
 	         	_this.outTradeNoAli = this.$route.query.out_trade_no;
 	         	var productId = _this.isCur + 1;
@@ -217,6 +207,7 @@
 						if(response.data.code == 0){
 							if(response.data.object.status == 1){
 								_this.paySuccessMask = true;
+								window.history.replaceState(null, "充值", "#/recharge");
 								// setTimeout(function(){
 								// 	_this.paySuccessMask = false;
 								// 	window.location.href = '#/recharge';
